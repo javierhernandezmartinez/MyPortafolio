a68404e4b91bb0d55347b12a7344563fcb72c56f
HERNANDEZ <javier_9509@hotmail.com>
2022-08-09 18:40:23 -0500

    starting portafolio

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePrincipal msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePrincipal from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePrincipal
   }
 }
 </script>
